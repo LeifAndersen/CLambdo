3f6921608db080aff4b4041133104523290523f3
Leif Andersen <leif@leifandersen.net>
2013-11-14 14:16:46 -0700

    Finished interpreter.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject CLambdo "1.0.0-SNAPSHOT"
   :main clambdo.core
   :description "A lambda calculus interpreter in clojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0"]])
--- src/clambdo/core.clj
+++ src/clambdo/core.clj
@@ -1,6 +1,35 @@
 (ns clambdo.core
   (:gen-class :main true))
 
+(declare inject)
+(declare leval)
+(declare lapply)
+
+(use '[clojure.core.match :only (match)])
+
+(defn leval [e env]
+  (match [e]
+    [s :guard symbol?] (env s)
+    [(['λ v b] :seq)]  `(closure ~e ~env)
+    [([f v] :seq)]     #(lapply (leval f env) (leval v env))
+    :else              (println "Error in leval!")))
+
+(defn lapply [f e]
+  (match [f]
+    [([closure (['λ v b] :seq) env] :seq)] #(leval b (conj env {v e}))
+    :else     (let []
+                (println "Error in lapply!")
+                (print "f: ") (println f) (newline) (print "e: ") (println e))))
+
+(defn inject [code]
+  (trampoline leval code {}))
+
+(def test-exp
+  '((λ x x) (λ x x)))
+
+(def omega
+  '((λ x (x x)) (λ x (x x))))
+
 (defn -main []
-  (println "Hello World!"))
+  (println (inject test-exp)))
 
